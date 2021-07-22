d78618237340451650978ac6dae1c7faaf629f7c
AhmetBolat <ahmet.bolat@invicti.com>
2021-07-22 10:03:11 +0300

    noimage

--- app/src/DB/MakarnaMenu.ts
+++ app/src/DB/MakarnaMenu.ts
@@ -1,7 +1,7 @@
 import { MenuModel } from "../Model/MenuModel";
 
 export const MakarnaMenu: MenuModel[] = [
-    { Title: "Fettuccine Alfredo", Description: "Fettuccine Alfredo", ImageSrc: './../../../images/noimage.jpg', RedirectUrl: "Makarna" },
-    { Title: "Panne Arabiatta", Description: "Panne Arabiatta", ImageSrc: './../../../images/noimage.jpg', RedirectUrl: "Makarna" },
-    { Title: "Spaghetti Bolognese", Description: "Spaghetti Bolognese", ImageSrc: './../../../images/noimage.jpg', RedirectUrl: "Makarna" },
+    { Title: "Fettuccine Alfredo", Description: "Fettuccine Alfredo", ImageSrc: './../../../images/noimage.jpeg', RedirectUrl: "Makarna" },
+    { Title: "Panne Arabiatta", Description: "Panne Arabiatta", ImageSrc: './../../../images/noimage.jpeg', RedirectUrl: "Makarna" },
+    { Title: "Spaghetti Bolognese", Description: "Spaghetti Bolognese", ImageSrc: './../../../images/noimage.jpeg', RedirectUrl: "Makarna" },
 ];
--- app/src/DB/SicakIcecekMenu.ts
+++ app/src/DB/SicakIcecekMenu.ts
@@ -1,11 +1,11 @@
 import { MenuModel } from "../Model/MenuModel";
 
 export const SicakIcecekMenu: MenuModel[] = [
-    { Title: "Çay", Description: "", ImageSrc: './../../../images/sicakicecek.jpg', RedirectUrl: "SicakIcecek" },
-    { Title: "Oralet", Description: "", ImageSrc: './../../../images/noimage.jpg', RedirectUrl: "SicakIcecek" },
-    { Title: "Kahve", Description: "", ImageSrc: './../../../images/noimage.jpg', RedirectUrl: "SicakIcecek" },
-    { Title: "Kaynayan Kahve", Description: "", ImageSrc: './../../../images/noimage.jpg', RedirectUrl: "SicakIcecek" },
-    { Title: "Nescafe", Description: "", ImageSrc: './../../../images/noimage.jpg', RedirectUrl: "SicakIcecek" },
-    { Title: "Sahlep", Description: "", ImageSrc: './../../../images/noimage.jpg', RedirectUrl: "SicakIcecek" },
-    { Title: "Sıcak Çikolata", Description: "", ImageSrc: './../../../images/noimage.jpg', RedirectUrl: "SicakIcecek" }
+    { Title: "Çay", Description: "", ImageSrc: './../../../images/sicakicecek.jpeg', RedirectUrl: "SicakIcecek" },
+    { Title: "Oralet", Description: "", ImageSrc: './../../../images/noimage.jpeg', RedirectUrl: "SicakIcecek" },
+    { Title: "Kahve", Description: "", ImageSrc: './../../../images/noimage.jpeg', RedirectUrl: "SicakIcecek" },
+    { Title: "Kaynayan Kahve", Description: "", ImageSrc: './../../../images/noimage.jpeg', RedirectUrl: "SicakIcecek" },
+    { Title: "Nescafe", Description: "", ImageSrc: './../../../images/noimage.jpeg', RedirectUrl: "SicakIcecek" },
+    { Title: "Sahlep", Description: "", ImageSrc: './../../../images/noimage.jpeg', RedirectUrl: "SicakIcecek" },
+    { Title: "Sıcak Çikolata", Description: "", ImageSrc: './../../../images/noimage.jpeg', RedirectUrl: "SicakIcecek" }
 ];
