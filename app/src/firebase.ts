0103c80c5dfa4cde84c561fefac3cd1574947e53
AhmetBolat <ahmet.bolat@invicti.com>
2021-07-27 02:37:10 +0300

    remove console log

--- app/src/firebase.ts
+++ app/src/firebase.ts
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/database';
 
-console.log(JSON.parse(process.env.FIREBASE_CONFIG));
 firebase.initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
 const database = firebase.database();
 
